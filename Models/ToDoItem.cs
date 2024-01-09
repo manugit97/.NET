c402322fd3a46fd813dd6023d0a6c4120331e2cd
manugit97 <156077517+manugit97@users.noreply.github.com>
2024-01-09 11:40:08 +0100

    Add files via upload

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,7 @@
+namespace ToDoWebApi.Models;
+
+    public class ToDoItem {
+        public long Id {get; set;}
+        public string Name {get; set;}
+        public bool Completed {get; set;}
+    }
