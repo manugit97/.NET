c0484331f6a3ac2ce51d7a41222c4c47815629a5
manugit97 <156077517+manugit97@users.noreply.github.com>
2024-01-25 16:16:13 +0100

    Delete EsercizioWebSwagger directory

--- EsercizioWebSwagger/Models/ToDoItemList.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace ToDoWebApi.Models;
-
-    public class ToDoItemList {
-        public long Id {get; set;}
-        public string Name {get; set;}
-        public List<ToDoItem> ToDoItems {get; set;}
-    }
